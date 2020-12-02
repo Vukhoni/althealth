b800f6c8370037b1e706fb9452d4c36c8f5ae518
Vukhoni <40644995@mylife.unisa.ac.za>
2020-12-03 00:50:06 +0200

    Daily Backup

--- althealth/dump.sql
+++ althealth/dump.sql
@@ -732,4 +732,4 @@ USE `althealth`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-03  0:47:23
+-- Dump completed on 2020-12-03  0:50:06
